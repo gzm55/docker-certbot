1a25a3ac658827d2916d242c1af87553d285b423
James Z.M. Gao <gaozhiming@360.cn>
2017-07-21 17:36:35 +0800

    update known subcommands

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -el
 
 case "$1" in
-(-*|run|certonly|auth|install|renew|revoke|register|rollback|config_changes|plugins)
+(-*|run|certonly|certificates|install|renew|revoke|register|rollback|config_changes|plugins|delete|unregister|update_symlinks)
   # certbot subcommands
   exec /usr/bin/env certbot "$@"
   ;;
